1a2d7bbc8d9e0ae01beb5ab13092ef632d366337
nico877h <Nico877h@elev.tec.dk>
2024-05-01 15:24:34 +0200

    black screen

---
+++ kode/board.py
@@ -0,0 +1,7 @@
+import pygame
+import værdier
+
+
+def board():
+    canvas = pygame.display.set_mode((500, 500))
+
--- kode/main.py
+++ kode/main.py
@@ -1,1 +1,33 @@
 import pygame
+import board
+import brikker 
+import menu
+import værdier
+
+pygame.init()
+
+size = (500,500)
+
+
+canvas = pygame.display.set_mode((size))
+pygame.display.set_caption("Himmelsten")
+
+
+
+
+def main():
+    run=True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+     
+    pygame.display.update()
+
+main()
+
+
+
+
+
+pygame.quit()
