7ef1c824837a068452c93eeaaa59d3909c45544e
yahoo101 <115617806+yahoo101@users.noreply.github.com>
2024-05-13 10:57:16 +0200

    test
    test

---
+++ kode/test.py
@@ -0,0 +1,16 @@
+#chose brick
+for x in input:
+    input = playerchoise
+    if playerchoise >= 1 and x in brikker:
+        return x in input and print(x)
+    else:
+        print("Input has to be 1 to 5")
+    
+
+#place brick
+for x in input:
+    input = playerchoise
+    if playerchoise >= 1 and x in felter:
+        return x in input and print(x)
+    else:
+        print("Input has to be 1 to 9")
