21677bbe9efe5d1e3fe30250de669365d80f736a
nico877h <Nico877h@elev.tec.dk>
2024-05-14 10:08:19 +0200

    print image

--- kode/main.py
+++ kode/main.py
@@ -27,20 +27,19 @@ display = pygame.display.set_mode((width, height))
 ###for x in board.felter:
     ###pygame.draw.rect(display, grey, x)
 
-imageSize = (150, 150)
-square = pygame.image.load("square.png").convert_alpha()
-square = pygame.transform.scale(square, imageSize)
+imageSize = (CellSize, CellSize)
+square_img = pygame.image.load("square.png").convert_alpha()
+square_img = pygame.transform.scale(square_img, imageSize)
 
 
 def draw_grid(x_offset, y_offset, rows, collums):
     for row in range(rows):
         for collum in range(collums):
             rect = pygame.Rect(x_offset + collum * (CellSize + GridSpaceing),
-                               y_offset + row * (CellSize + GridSpaceing),
-                               CellSize, CellSize)
+                               y_offset + row * (CellSize + GridSpaceing), CellSize, CellSize)
             pygame.draw.rect(display, White, rect, gridSize)
 
-
+#Draw 5 cards from deck list
 def draw_cards():
     if len(brikker.deck) >= 5:
         return random.sample(brikker.deck, 5)
@@ -48,21 +47,21 @@ def draw_cards():
         print("not enough cards in deck")
         return None
 
-
+#use Draw card function
 player1_cards = draw_cards()
 if player1_cards:
     print("player1 drew")
     for card in player1_cards:
         print("-",card)
         
-
+#use Draw card function
 player2_cards = draw_cards()
 if player2_cards:
     print("player2 drew")
     for card in player2_cards:
         print("-",card)
 
-
+#main
 def play_himmelsten():
     run=True
 
@@ -85,11 +84,14 @@ def play_himmelsten():
         right_x = center_x + (gridSize * CellSize) + GridSpaceing*3
         draw_grid(right_x, left_y, sideGridSize, 1)
 
-        display.blit(square, (left_x,left_y))
         
-        pygame.display.flip()
+        for iterator, square in enumerate(player1_cards):
+            display.blit(square_img, (left_x,left_y+iterator*(CellSize+GridSpaceing)))
+        
+        for iterator, square in enumerate(player2_cards):
+            display.blit(square_img, (right_x,left_y+iterator*(CellSize+GridSpaceing)))
+        
     
-        
         pygame.display.update()
     
 
--- kode/test.py
+++ kode/test.py
@@ -1,11 +1,14 @@
+from brikker import Brikker
+
+
 #chose brick
 for x in input:
     input = playerchoise
-    if playerchoise >= 1 and x in brikker:
+    if playerchoise >= 1 and x in Brikker.deck:
         return x in input and print(x)
     else:
         print("Input has to be 1 to 5")
-    
+
 
 #place brick
 for x in input:
@@ -14,3 +17,5 @@ for x in input:
         return x in input and print(x)
     else:
         print("Input has to be 1 to 9")
+
+print(input)
