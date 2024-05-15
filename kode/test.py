8cb2af2ff8d53956bc7ddd68322f001ba42aba59
nico877h <Nico877h@elev.tec.dk>
2024-05-15 11:55:01 +0200

    working hands

--- kode/test.py
+++ kode/test.py
@@ -4,26 +4,31 @@ from brikker import Brikker
 hand_1 = ["1","2","3","4","5"]
 hand_2 = ["1","2","3","4","5"]
 board = ["1","2","3","4","5","6","7","8","9"]
+felter = {1: "",2: "",3: "",4: "",5: "",6: "",7: "",8: "",9: ""}
 #chose brick
 chosenbrick = None
-user_input = input("which piece?") #Example "1" -> DrowlanceMaster
-for piece in hand:
-    if user_input == piece: # tildel de 5 forskellige brikker en værdi fra 1 til 5
-        chosenbrick = piece
-        hand.remove(piece)
-    else:
-        if input not in list.hand: 
+user_input = int(input("which piece?"))#Example "1" -> DrowlanceMaster
+if user_input not in range(1, 6): 
             print("Input has to be 1 to 5")
+else:
+    for piece in range(1, 6):
+        if user_input == piece: # tildel de 5 forskellige brikker en værdi fra 1 til 5
+            chosenbrick = piece
+
 
 #chose collum
 chosenfelt = None
-user_input = input("which collum?") #Example "1" -> collum 1
-for felt in board:
-    if user_input == felt and felt == empty: # vælger et felt og ligge brikken på
-        chosenfelt = felt
-    else:
-        if input not in list.board: 
-            print("Input has to be 1 to 5")
+user_input = int(input("which collum?")) #Example "1" -> collum 1
+if user_input not in range(1, 10): 
+            print("Input has to be 1 to 9")
+
+for key, felt in felter.items():
+    print(user_input == key)
+    if user_input == key and felt == "": # vælger et felt og ligge brikken på
+        felter[key] = chosenbrick
 
 
-print(input)
+
+
+
+print(felter)
